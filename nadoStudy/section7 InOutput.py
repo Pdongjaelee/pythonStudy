27354c7070e0567090a1211bdce15efe57ac760b
Pdongjae <kyp4810@gmail.com>
2023-04-24 19:24:55 +0900

    python study (38/71), S7 start(230424)

---
+++ nadoStudy/section7 InOutput.py
@@ -0,0 +1,17 @@
+###section7 입출력
+
+
+## 표준입출력
+# print("Python", "Java","js", sep=" , ", end="?") #sep를 통해 콤마를 할지 띄어쓰기를 할지 지정해줄 수 있다.
+# print("무엇이 더 재밌을까요?") #end에 ?가 오면 다음에 오는 문장을 연달아서 출력한다.
+
+# import sys
+# print("Python", "Java",file=sys.stdout) #표준출력
+# print("Python", "Java",file=sys.stderr) #표준에러로 출력
+
+# 시험 성적
+scores = {"수학":0, "영어":50, "코딩":100}
+fo
+
+
+
