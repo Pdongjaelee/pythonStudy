50aefb30eb9b0ce06277a6a92253cfadec80a30a
Pdongjae <kyp4810@gmail.com>
2023-04-25 19:43:21 +0900

    python study (39/71), S7(230425)

--- nadoStudy/section7 InOutput.py
+++ nadoStudy/section7 InOutput.py
@@ -10,8 +10,34 @@
 # print("Python", "Java",file=sys.stderr) #표준에러로 출력
 
 # 시험 성적
-scores = {"수학":0, "영어":50, "코딩":100}
-fo
+# scores = {"수학":0, "영어":50, "코딩":100}
+# for subject, score in scores.items():
+#     #print(subject, score)
+#     print(subject.ljust(8), str(score).rjust(4), sep=":")
 
+
+# 은행 대기순번표
+# 001, 002, 003, ...
+# for num in range(1, 21):
+#     print("대기번호 : "+ str(num).zfill(3))
+#
+# # answer = input("아무 값이나 입력하세요 :") # 사용자 입력을 통해서 값을 받게 되면 항상 문자열 형태로 받게된다.
+# answer = 10
+# print(type(answer))
+# print("입력하신 값은" + answer + "입니다.")
+
+
+
+## 다양한 출력포멧
+
+# 빈 자리는 빈공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간을 확보
+print("{0: >10}".format(500))
+# 양수일 떈 + 표시, 음수일 땐 -로 표시
+print("{0: >+10}".format(500))
+print("{0: >+10}".format(-500))
+# 왼쪽 정렬하고, 빈칸으로 _로 채움
+print("{0:_<10}".format(500))
+# 3자리 마다 콤마를 찍어주기
+print("{0:,}".format(100000000000))
 
 
